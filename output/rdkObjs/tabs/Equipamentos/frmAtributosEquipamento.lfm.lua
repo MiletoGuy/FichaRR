31b3a9d5aa5f4a1c1715df50230076738fdcc421
Tales <tales5c@outlook.com>
2024-09-13 14:40:39 -0300

    fazendo o componente dinamico

--- output/rdkObjs/tabs/Equipamentos/frmAtributosEquipamento.lfm.lua
+++ output/rdkObjs/tabs/Equipamentos/frmAtributosEquipamento.lfm.lua
@@ -38,19 +38,118 @@ local function constructNew_frmAtributosEquipamento()
     obj.layout1:setMargins({top=2, bottom=2});
     obj.layout1:setName("layout1");
 
-    obj.retanguloVerde = GUI.fromHandle(_obj_newObject("rectangle"));
-    obj.retanguloVerde:setParent(obj.layout1);
-    obj.retanguloVerde:setName("retanguloVerde");
-    obj.retanguloVerde:setAlign("client");
-    obj.retanguloVerde:setColor("green");
-    obj.retanguloVerde:setVisible(false);
+    obj.atributoFlat = GUI.fromHandle(_obj_newObject("layout"));
+    obj.atributoFlat:setParent(obj.layout1);
+    obj.atributoFlat:setName("atributoFlat");
+    obj.atributoFlat.grid.role = "row";
+    obj.atributoFlat.grid["min-height"] = 30;
+    obj.atributoFlat.grid["cnt-horz-align"] = "center";
+    obj.atributoFlat.grid["cnt-vert-align"] = "center";
+    obj.atributoFlat:setMargins({top=2, bottom=2});
+    obj.atributoFlat.grid["cnt-gutter"] = 10;
 
-    obj.retanguloAmarelo = GUI.fromHandle(_obj_newObject("rectangle"));
-    obj.retanguloAmarelo:setParent(obj.layout1);
-    obj.retanguloAmarelo:setName("retanguloAmarelo");
-    obj.retanguloAmarelo:setAlign("client");
-    obj.retanguloAmarelo:setColor("yellow");
-    obj.retanguloAmarelo:setVisible(false);
+    obj.label1 = GUI.fromHandle(_obj_newObject("label"));
+    obj.label1:setParent(obj.atributoFlat);
+    obj.label1.grid.role = "block";
+    obj.label1:setText("+");
+    obj.label1:setWidth(10);
+    obj.label1:setName("label1");
+
+    obj.edit1 = GUI.fromHandle(_obj_newObject("edit"));
+    obj.edit1:setParent(obj.atributoFlat);
+    obj.edit1.grid.role = "col";
+    obj.edit1:setField("valorAtributo");
+    obj.edit1:setHorzTextAlign("center");
+    obj.edit1:setName("edit1");
+
+    obj.label2 = GUI.fromHandle(_obj_newObject("label"));
+    obj.label2:setParent(obj.atributoFlat);
+    obj.label2.grid.role = "block";
+    obj.label2:setText("de");
+    obj.label2:setWidth(15);
+    obj.label2:setName("label2");
+
+    obj.comboBox1 = GUI.fromHandle(_obj_newObject("comboBox"));
+    obj.comboBox1:setParent(obj.atributoFlat);
+    obj.comboBox1.grid.role = "col";
+    obj.comboBox1:setField("nomeAtributo");
+    obj.comboBox1:setItems({'Constituição', 'Força', 'Destreza', 'Carisma', 'Inteligência', 'Sabedoria', 'Acerto',
+'Mira', 'Esquiva', 'Bloqueio'});
+    obj.comboBox1:setValues({'con', 'for', 'dex', 'car', 'int', 'sab', 'acerto', 'mira', 'esquiva', 'bloqueio'});
+    obj.comboBox1:setName("comboBox1");
+
+    obj.comboBox2 = GUI.fromHandle(_obj_newObject("comboBox"));
+    obj.comboBox2:setParent(obj.atributoFlat);
+    obj.comboBox2:setField("radioTeste");
+    obj.comboBox2.grid.role = "col";
+    obj.comboBox2.grid.width = 1;
+    obj.comboBox2:setItems({'Flat', 'Porcentagem', 'Vermelho'});
+    obj.comboBox2:setValues({'flat', 'porcentagem', 'vermelho'});
+    obj.comboBox2:setName("comboBox2");
+
+    obj.button1 = GUI.fromHandle(_obj_newObject("button"));
+    obj.button1:setParent(obj.atributoFlat);
+    obj.button1:setText("X");
+    obj.button1.grid.role = "block";
+    obj.button1:setWidth(20);
+    obj.button1:setName("button1");
+
+    obj.atributoPorcentagem = GUI.fromHandle(_obj_newObject("layout"));
+    obj.atributoPorcentagem:setParent(obj.layout1);
+    obj.atributoPorcentagem:setName("atributoPorcentagem");
+    obj.atributoPorcentagem.grid.role = "row";
+    obj.atributoPorcentagem.grid["min-height"] = 30;
+    obj.atributoPorcentagem.grid["cnt-horz-align"] = "center";
+    obj.atributoPorcentagem.grid["cnt-vert-align"] = "center";
+    obj.atributoPorcentagem:setVisible(false);
+    obj.atributoPorcentagem:setMargins({top=2, bottom=2});
+    obj.atributoPorcentagem.grid["cnt-gutter"] = 10;
+
+    obj.label3 = GUI.fromHandle(_obj_newObject("label"));
+    obj.label3:setParent(obj.atributoPorcentagem);
+    obj.label3.grid.role = "block";
+    obj.label3:setText("+");
+    obj.label3:setWidth(10);
+    obj.label3:setName("label3");
+
+    obj.edit2 = GUI.fromHandle(_obj_newObject("edit"));
+    obj.edit2:setParent(obj.atributoPorcentagem);
+    obj.edit2.grid.role = "col";
+    obj.edit2:setField("valorAtributo");
+    obj.edit2:setHorzTextAlign("center");
+    obj.edit2:setName("edit2");
+
+    obj.label4 = GUI.fromHandle(_obj_newObject("label"));
+    obj.label4:setParent(obj.atributoPorcentagem);
+    obj.label4.grid.role = "block";
+    obj.label4:setText("% de");
+    obj.label4:setWidth(30);
+    obj.label4:setName("label4");
+
+    obj.comboBox3 = GUI.fromHandle(_obj_newObject("comboBox"));
+    obj.comboBox3:setParent(obj.atributoPorcentagem);
+    obj.comboBox3.grid.role = "col";
+    obj.comboBox3:setField("nomeAtributo");
+    obj.comboBox3:setItems({'Constituição', 'Força', 'Destreza', 'Carisma', 'Inteligência', 'Sabedoria', 'Acerto',
+'Mira', 'Esquiva', 'Bloqueio'});
+    obj.comboBox3:setValues({'con', 'for', 'dex', 'car', 'int', 'sab', 'acerto', 'mira', 'esquiva', 'bloqueio'});
+    obj.comboBox3:setName("comboBox3");
+
+    obj.comboBox4 = GUI.fromHandle(_obj_newObject("comboBox"));
+    obj.comboBox4:setParent(obj.atributoPorcentagem);
+    obj.comboBox4:setField("radioTeste");
+    obj.comboBox4.grid.role = "col";
+    obj.comboBox4.grid.width = 1;
+    obj.comboBox4:setItems({'Flat', 'Porcentagem', 'Vermelho'});
+    obj.comboBox4:setValues({'flat', 'porcentagem', 'vermelho'});
+    obj.comboBox4:setName("comboBox4");
+
+    obj.button2 = GUI.fromHandle(_obj_newObject("button"));
+    obj.button2:setParent(obj.atributoPorcentagem);
+    obj.button2:setText("X");
+    obj.button2.grid.role = "block";
+    obj.button2:setWidth(20);
+    obj.button2:setName("button2");
 
     obj.retanguloVermelho = GUI.fromHandle(_obj_newObject("rectangle"));
     obj.retanguloVermelho:setParent(obj.layout1);
@@ -59,96 +158,48 @@ local function constructNew_frmAtributosEquipamento()
     obj.retanguloVermelho:setColor("red");
     obj.retanguloVermelho:setVisible(false);
 
-    obj.botaoOpcoes = GUI.fromHandle(_obj_newObject("button"));
-    obj.botaoOpcoes:setParent(obj.layout1);
-    obj.botaoOpcoes:setName("botaoOpcoes");
-    obj.botaoOpcoes:setText("🛈");
-
-    obj.popOpcoesRetangulo = GUI.fromHandle(_obj_newObject("popup"));
-    obj.popOpcoesRetangulo:setParent(obj.layout1);
-    obj.popOpcoesRetangulo:setName("popOpcoesRetangulo");
-    obj.popOpcoesRetangulo:setWidth(100);
-    obj.popOpcoesRetangulo:setHeight(100);
-
-    obj.radioButton1 = GUI.fromHandle(_obj_newObject("radioButton"));
-    obj.radioButton1:setParent(obj.popOpcoesRetangulo);
-    obj.radioButton1:setText("verde");
-    obj.radioButton1:setGroupName("grupo1");
-    obj.radioButton1:setTop(5);
-    obj.radioButton1:setField("radioTeste");
-    obj.radioButton1:setFieldValue("verde");
-    obj.radioButton1:setName("radioButton1");
-
-    obj.radioButton2 = GUI.fromHandle(_obj_newObject("radioButton"));
-    obj.radioButton2:setParent(obj.popOpcoesRetangulo);
-    obj.radioButton2:setText("vermelho");
-    obj.radioButton2:setGroupName("grupo1");
-    obj.radioButton2:setTop(25);
-    obj.radioButton2:setField("radioTeste");
-    obj.radioButton2:setFieldValue("vermelho");
-    obj.radioButton2:setName("radioButton2");
-
-    obj.radioButton3 = GUI.fromHandle(_obj_newObject("radioButton"));
-    obj.radioButton3:setParent(obj.popOpcoesRetangulo);
-    obj.radioButton3:setText("amarelo");
-    obj.radioButton3:setGroupName("grupo1");
-    obj.radioButton3:setTop(45);
-    obj.radioButton3:setField("radioTeste");
-    obj.radioButton3:setFieldValue("amarelo");
-    obj.radioButton3:setName("radioButton3");
+    obj.comboBox5 = GUI.fromHandle(_obj_newObject("comboBox"));
+    obj.comboBox5:setParent(obj.retanguloVermelho);
+    obj.comboBox5:setField("radioTeste");
+    obj.comboBox5.grid.role = "col";
+    obj.comboBox5.grid.width = 1;
+    obj.comboBox5:setItems({'Flat', 'Porcentagem', 'Vermelho'});
+    obj.comboBox5:setValues({'flat', 'porcentagem', 'vermelho'});
+    obj.comboBox5:setName("comboBox5");
 
     obj.dataLink1 = GUI.fromHandle(_obj_newObject("dataLink"));
     obj.dataLink1:setParent(obj);
     obj.dataLink1:setField("radioTeste");
     obj.dataLink1:setName("dataLink1");
 
-
-            function atualizaRetangulos()
-                if sheet.radioTeste == "verde" then
-                    self.retanguloVerde.visible = true
-                    self.retanguloAmarelo.visible = false
-                    self.retanguloVermelho.visible = false
-                elseif sheet.radioTeste == "amarelo" then
-                    self.retanguloVerde.visible = false
-                    self.retanguloAmarelo.visible = true
-                    self.retanguloVermelho.visible = false
-                elseif sheet.radioTeste == "vermelho" then
-                    self.retanguloVerde.visible = false
-                    self.retanguloAmarelo.visible = false
-                    self.retanguloVermelho.visible = true
-                end
-            end
-            
-
-
-    obj._e_event0 = obj.botaoOpcoes:addEventListener("onClick",
+    obj._e_event0 = obj.button1:addEventListener("onClick",
         function (event)
-            self.popOpcoesRetangulo:show('bottom', self.botaoOpcoes)
+            NDB.deleteNode(sheet);
         end);
 
-    obj._e_event1 = obj.radioButton1:addEventListener("onChange",
-        function ()
-            atualizaRetangulos(); self.popOpcoesRetangulo:close()
+    obj._e_event1 = obj.button2:addEventListener("onClick",
+        function (event)
+            NDB.deleteNode(sheet);
         end);
 
-    obj._e_event2 = obj.radioButton2:addEventListener("onChange",
-        function ()
-            atualizaRetangulos(); self.popOpcoesRetangulo:close()
-        end);
-
-    obj._e_event3 = obj.radioButton3:addEventListener("onChange",
-        function ()
-            atualizaRetangulos(); self.popOpcoesRetangulo:close()
-        end);
-
-    obj._e_event4 = obj.dataLink1:addEventListener("onChange",
+    obj._e_event2 = obj.dataLink1:addEventListener("onChange",
         function (field, oldValue, newValue)
-            atualizaRetangulos()
+            if sheet.radioTeste == "flat" then
+                        self.atributoFlat.visible = true
+                        self.atributoPorcentagem.visible = false
+                        self.retanguloVermelho.visible = false
+                        elseif sheet.radioTeste == "porcentagem" then
+                        self.atributoFlat.visible = false
+                        self.atributoPorcentagem.visible = true
+                        self.retanguloVermelho.visible = false
+                        elseif sheet.radioTeste == "vermelho" then
+                        self.atributoFlat.visible = false
+                        self.atributoPorcentagem.visible = false
+                        self.retanguloVermelho.visible = true
+                        end
         end);
 
     function obj:_releaseEvents()
-        __o_rrpgObjs.removeEventListenerById(self._e_event4);
-        __o_rrpgObjs.removeEventListenerById(self._e_event3);
         __o_rrpgObjs.removeEventListenerById(self._e_event2);
         __o_rrpgObjs.removeEventListenerById(self._e_event1);
         __o_rrpgObjs.removeEventListenerById(self._e_event0);
@@ -163,15 +214,23 @@ local function constructNew_frmAtributosEquipamento()
           self:setNodeDatabase(nil);
         end;
 
-        if self.radioButton3 ~= nil then self.radioButton3:destroy(); self.radioButton3 = nil; end;
-        if self.popOpcoesRetangulo ~= nil then self.popOpcoesRetangulo:destroy(); self.popOpcoesRetangulo = nil; end;
-        if self.radioButton1 ~= nil then self.radioButton1:destroy(); self.radioButton1 = nil; end;
+        if self.label2 ~= nil then self.label2:destroy(); self.label2 = nil; end;
+        if self.button2 ~= nil then self.button2:destroy(); self.button2 = nil; end;
         if self.dataLink1 ~= nil then self.dataLink1:destroy(); self.dataLink1 = nil; end;
-        if self.botaoOpcoes ~= nil then self.botaoOpcoes:destroy(); self.botaoOpcoes = nil; end;
-        if self.retanguloVerde ~= nil then self.retanguloVerde:destroy(); self.retanguloVerde = nil; end;
-        if self.retanguloAmarelo ~= nil then self.retanguloAmarelo:destroy(); self.retanguloAmarelo = nil; end;
+        if self.comboBox2 ~= nil then self.comboBox2:destroy(); self.comboBox2 = nil; end;
+        if self.label1 ~= nil then self.label1:destroy(); self.label1 = nil; end;
+        if self.button1 ~= nil then self.button1:destroy(); self.button1 = nil; end;
+        if self.comboBox3 ~= nil then self.comboBox3:destroy(); self.comboBox3 = nil; end;
+        if self.atributoFlat ~= nil then self.atributoFlat:destroy(); self.atributoFlat = nil; end;
+        if self.atributoPorcentagem ~= nil then self.atributoPorcentagem:destroy(); self.atributoPorcentagem = nil; end;
+        if self.comboBox4 ~= nil then self.comboBox4:destroy(); self.comboBox4 = nil; end;
+        if self.edit2 ~= nil then self.edit2:destroy(); self.edit2 = nil; end;
+        if self.label4 ~= nil then self.label4:destroy(); self.label4 = nil; end;
+        if self.edit1 ~= nil then self.edit1:destroy(); self.edit1 = nil; end;
+        if self.label3 ~= nil then self.label3:destroy(); self.label3 = nil; end;
         if self.retanguloVermelho ~= nil then self.retanguloVermelho:destroy(); self.retanguloVermelho = nil; end;
-        if self.radioButton2 ~= nil then self.radioButton2:destroy(); self.radioButton2 = nil; end;
+        if self.comboBox5 ~= nil then self.comboBox5:destroy(); self.comboBox5 = nil; end;
+        if self.comboBox1 ~= nil then self.comboBox1:destroy(); self.comboBox1 = nil; end;
         if self.layout1 ~= nil then self.layout1:destroy(); self.layout1 = nil; end;
         self:_oldLFMDestroy();
     end;
